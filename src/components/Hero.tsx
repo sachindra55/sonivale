92caf3b03c7fed243eef5612a07f53447d7f9f59
sachindra55 <51438007+sachindra55@users.noreply.github.com>
2025-02-03 09:12:28 +1100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
       </div>
       <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
-          Welcome to Sonivale
+          Welcome to Sonivale. A Learning Platoform
         </h1>
         <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
           A virtual community supporting authentic, meaningful, and contextualized learning
